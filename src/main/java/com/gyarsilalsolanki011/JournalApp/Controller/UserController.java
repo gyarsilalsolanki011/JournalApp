1720ead43aa3724fb49cd7a2b72d0b4de711f09c
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2024-07-15 16:32:38 +0530

    User Collection & Mapping Added

---
+++ src/main/java/com/gyarsilalsolanki011/JournalApp/Controller/UserController.java
@@ -0,0 +1,40 @@
+package com.gyarsilalsolanki011.JournalApp.Controller;
+
+import com.gyarsilalsolanki011.JournalApp.Entity.User;
+import com.gyarsilalsolanki011.JournalApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers(@RequestBody User user) {
+        return userService.grtAllUsers();
+    }
+
+    @PostMapping
+    public void createUser(@RequestBody User user) {
+        userService.saveEntry(user);
+    }
+
+    @PutMapping("/{userName}")
+    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable String userName) {
+        User userInDb = userService.findByUserName(userName);
+        if (userInDb != null) {
+            userInDb.setUserName(user.getUserName());
+            userInDb.setPassword(user.getPassword());
+            userService.saveEntry(userInDb);
+        }
+        return  new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
