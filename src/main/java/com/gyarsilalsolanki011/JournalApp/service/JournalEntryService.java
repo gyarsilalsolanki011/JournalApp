19a4806f912f0895d93aee3f62799001c0ba7913
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2024-07-15 20:40:00 +0530

    Mongo Transaction Manager Added

--- src/main/java/com/gyarsilalsolanki011/JournalApp/service/JournalEntryService.java
+++ src/main/java/com/gyarsilalsolanki011/JournalApp/service/JournalEntryService.java
@@ -6,6 +6,7 @@ import com.gyarsilalsolanki011.JournalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,11 +21,13 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName){
         User user = userService.findByUserName(userName);
         journalEntry.setDate(LocalDateTime.now());
         JournalEntry saved = journalEntryRepository.save(journalEntry);
         user.getJournalEntries().add(saved);
+        user.setUserName(null);
         userService.saveEntry(user);
     }
 
