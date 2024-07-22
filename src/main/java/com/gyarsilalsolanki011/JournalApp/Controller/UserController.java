1dc3405f842c929da2a3c69b73bc17b0c40799cf
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2024-07-22 23:17:07 +0530

    Some web security added

--- src/main/java/com/gyarsilalsolanki011/JournalApp/Controller/UserController.java
+++ src/main/java/com/gyarsilalsolanki011/JournalApp/Controller/UserController.java
@@ -5,36 +5,33 @@ import com.gyarsilalsolanki011.JournalApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
     @Autowired
     private UserService userService;
 
-    @GetMapping
-    public List<User> getAllUsers(@RequestBody User user) {
-        return userService.grtAllUsers();
-    }
-
-    @PostMapping
-    public void createUser(@RequestBody User user) {
-        userService.saveEntry(user);
-    }
-
-    @PutMapping("/{userName}")
-    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable String userName) {
+    @PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody User user) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
         User userInDb = userService.findByUserName(userName);
         if (userInDb != null) {
             userInDb.setUserName(user.getUserName());
             userInDb.setPassword(user.getPassword());
             userService.saveEntry(userInDb);
         }
-        return  new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return  new ResponseEntity<>(userInDb, HttpStatus.OK);
+    }
+
+    @GetMapping("/{userName}")
+    public ResponseEntity<?> getUserBYUserName(@PathVariable String userName) {
+        User user = userService.findByUserName(userName);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
 }
